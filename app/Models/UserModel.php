c2adfd1e14efb5f9838d1c4d9d2a51716592422c
febriantimayori <febriantimayori@gmail.com>
2024-03-05 17:51:42 +0700

    JS4_Praktikum 2.4 – Retreiving or Creating Models

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,5 +12,5 @@ class UserModel extends Model
     protected $primaryKey = 'user_id'; //mendefinisikan nama tabel yang digunakan oleh model ini
     protected $table = 'm_user'; //mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,11 +8,17 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>Jumlah Pengguna</th>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
             </tr>
             {{-- @foreach($users as $data) --}}
             <tr>
-                <td>{{ $data }}</td>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
             {{-- @endforeach --}}
         </table>
