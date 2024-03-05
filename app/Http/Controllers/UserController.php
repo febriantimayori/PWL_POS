43e87ce991d88ea184096a12ceb3df97927268a6
febriantimayori <febriantimayori@gmail.com>
2024-03-05 17:02:58 +0700

    JS4_Praktikum 2.1 – Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,17 +25,32 @@ class UserController extends Controller
         ];
         UserModel::insert($data); // tambahkan data ke tabel m_user*/
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
 
-        UserModel::create($data);
+        // UserModel::create($data);
 
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // // coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+
+        // $user = UserModel::find(1); // ambil data user dengan id 1
+
+        // $user = UserModel::where('level_id', 1)->first(); // ambil data user dengan level_id 1
+
+        // $user = UserModel::firstWhere('level_id', 1); // ambil data user dengan level_id 1
+        
+        // $user = UserModel::findOr(1, ['username', 'nama'], function() {
+        //     abort(404);
+        // });
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
+
         return view('user', ['data' => $user]);
     }
 }
