d85afd7188e7477eac22df93df209d686d6fad4d
febriantimayori <febriantimayori@gmail.com>
2024-03-06 10:15:50 +0700

    JS4_Praktikum 2.7 – Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,9 +8,15 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    // public function index() 
+    // {
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
+
     public function index() 
     {
-        $user = UserModel::all();
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
 
