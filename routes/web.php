b3317deb50b680621cf07844802aa43aa4db2a5a
febriantimayori <febriantimayori@gmail.com>
2024-03-26 20:13:20 +0700

    JS7_Praktikum 1 – Layouting AdminLTE & Praktikum 2 – Penerapan Layouting

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\POSController;
 
@@ -78,3 +79,6 @@ Route::prefix('level')->group(function () {
 
 //Route m_user
 Route::resource('m_user', POSController::class);
+
+//JS 7
+Route::get('/', [WelcomeController::class, 'index']);
